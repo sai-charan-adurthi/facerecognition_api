0739d237dcae7d19d2e26ee61013647c5911a338
Sai Charan Adurthi <saicharanadurthi@outlook.com>
2021-06-10 14:17:17 +0530

    updating database

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const image = require('./controllers/image');
 const db = knex({
   client: "pg",
   connection: {
-    host: "127.0.0.1",
+    host: "postgresql-shaped-10980",
     user: "saicharanadurthi",
     password: "",
     database: "smart-brain",
