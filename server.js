f6cdafa7211384d672f058f7c0700024f7913d14
Sai Charan Adurthi <saicharanadurthi@outlook.com>
2021-06-10 13:56:25 +0530

    using port from env file

--- server.js
+++ server.js
@@ -29,6 +29,6 @@ app.get("/profile/:id", (req, res) => { profile.handleProfile(req, res, db) })
 app.put("/image", (req, res) => { image.handleImage(req, res, db) })
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
 
-app.listen(8000, () => {
-  console.log("app is running on port 8000");
+app.listen(process.env.PORT || 8000, () => {
+  console.log(`app is running on port ${process.env.PORT}`);
 });
